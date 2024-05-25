e5682e57c16893fa21cc232d4e35f8b86953e450
ZharMeny <135447733+ZharMeny@users.noreply.github.com>
2024-05-25 16:04:14 +0000

    fix indent-offset defcustom form

--- cue-ts-mode.el
+++ cue-ts-mode.el
@@ -32,7 +32,7 @@
 
 (defcustom cue-ts-mode-indent-offset 8
   "Number of spaces for each indentation step in `cue-ts-mode'."
-  :version "0.1.0"
+  :package-version "0.1.0"
   :type 'integer
   :safe 'integerp
   :group 'cue)
