cb8917bc140d9ffeddd186b4ec2f3218dd27f0a2
ZharMeny <135447733+ZharMeny@users.noreply.github.com>
2024-07-23 11:15:18 +0000

    adjust indentation rules

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((indent-tabs-mode . nil)))
- (makefile-mode . ((indent-tabs-mode . t))))
+((nil . ((indent-tabs-mode . t)))
+ (lisp-data-mode . ((indent-tabs-mode . nil))))
